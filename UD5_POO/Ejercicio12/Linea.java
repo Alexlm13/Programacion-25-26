0bb67121e475afa6b1c1e95aa25697c73c3259e7
Alexlm13 <alopmar131@g.educaand.es>
2026-02-03 13:58:46 +0100

    Add files via upload

---
+++ UD5_POO/Ejercicio12/Linea.java
@@ -0,0 +1,15 @@
+package Ejercicio12;
+
+public class Linea {
+    private Punto p1, p2;
+
+    public Linea(Punto p1, Punto p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    @Override
+    public String toString() {
+        return "Línea formada por los puntos "+p1+" y "+p2;
+    }
+}
